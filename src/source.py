1bd807fe3c0008b2c0b5f801043362a6c31bae3e
Konstantin Althaus <althaus.konstantin.95@gmail.com>
2024-03-01 17:59:05 +0100

    feat: add tags with strengths

--- src/source.py
+++ src/source.py
@@ -5,7 +5,13 @@ from re import findall
 from jinja2 import FileSystemLoader, Environment
 import tempfile
 import subprocess
-from constants import SECTIONS
+from constants import BIBLIOGRAPHY_TEX_FILE, PERSONAL_INFO_TEX_FILE, SECTIONS
+from constants import PERSONAL_INFO_TEMPLATE
+from constants import BIBLIOGRAPHY_TEMPLATE
+from constants import TAGS_FILE
+from constants import TAGS_TEX_FILE
+from constants import TAGS_TYPES
+from constants import TAGS_SUBTYPES
 from constants import BIBLIOGRAPHY
 from constants import VCARD
 from constants import PHOTO
@@ -48,19 +54,51 @@ def make_source_files():
     # make personal information
     personal_info = parser_personal_info(VCARD, PHOTO)
     fill_template(
-        Path("templates/personal_info_template.tex"),
+        PERSONAL_INFO_TEMPLATE,
         personal_info,
-        TEX_DIR / "personal_info.tex",
+        PERSONAL_INFO_TEX_FILE,
     )
     # make bibliography
     fill_template(
-        Path("templates/bibliography_template.tex"),
+        BIBLIOGRAPHY_TEMPLATE,
         {"bibliography": BIBLIOGRAPHY},
-        TEX_DIR / "bibliography.tex",
+        BIBLIOGRAPHY_TEX_FILE,
     )
-    # make sections
+    # make tags
+    make_tags_tex(TAGS_FILE, TAGS_TEX_FILE, TAGS_TYPES, TAGS_SUBTYPES)
+    # make main sections
     for sec in SECTIONS:
         csv_to_tex(sec, DATA_DIR, TEX_DIR)
+
+
+def make_tags_tex(tags_file: Path, tex_file: Path, tag_types: list, tag_subtypes: list):
+    tags = pd.read_csv(tags_file)
+    tags_by_type = tags.groupby(by="type")
+    tex_output = ""
+    for tag_type in tag_types:
+        if tag_type not in tags_by_type.groups:
+            continue
+        group = tags_by_type.get_group(tag_type)
+        group = group.groupby(by="subtype")
+        section_title = f"\\cvsection{{ {tag_type} }}"
+        subgroup_content_list = []
+        for tag_subtype in tag_subtypes:
+            if tag_subtype not in group.groups:
+                continue
+            subgroup = group.get_group(tag_subtype)
+            subgroup["tex_code"] = subgroup.apply(row_to_tex_code,
+                                                  axis=1,
+                                                  latex_command="cvtag")
+            subgroup = subgroup.sort_values(by="importance", ascending=False)
+            subgroup_content = "\n".join(subgroup.tex_code.values)
+            subgroup_content_list.append(subgroup_content)
+        section_content = "\n\\medskip\n\n".join(subgroup_content_list)
+        if not tex_output:
+            tex_output = "\n".join([section_title, section_content])
+        else:
+            tex_output = "\n".join([tex_output, "\\medskip\n\n", section_title, section_content])
+    with open(tex_file, "w") as f:
+        f.write(tex_output)
 
 
 def parser_personal_info(path_to_vcard: Path, path_to_photo: Path) -> dict:
@@ -121,8 +159,15 @@ def row_to_tex_code(row, latex_command="cvevent"):
         return make_cvevent(row)
     if latex_command == "cvproject":
         return make_cvproject(row)
+    if latex_command == "cvtag":
+        return make_cvtag(row)
     else:
         return ""
+
+
+def make_cvtag(row):
+    tag = f"\\cvtag{{ {row.title} }}"
+    return tag
 
 
 def make_cvproject(row):
