a56bc9f3a25b7e6052e5f3b74ce9db0a6e3d78dc
Konstantin Althaus <althaus.konstantin.95@gmail.com>
2024-04-12 16:09:55 +0200

    refactor(data): move date to yaml files

--- src/source.py
+++ src/source.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from re import sub
+from yaml import safe_load
 import vobject
 from pyshorteners import Shortener
 from re import findall
@@ -68,11 +70,13 @@ def make_source_files():
     make_tags_tex(TAGS_FILE, TAGS_TEX_FILE, TAGS_TYPES, TAGS_SUBTYPES)
     # make main sections
     for sec in SECTIONS:
-        csv_to_tex(sec, DATA_DIR, TEX_DIR)
+        yaml_to_tex(sec, DATA_DIR, TEX_DIR)
 
 
 def make_tags_tex(tags_file: Path, tex_file: Path, tag_types: list, tag_subtypes: list):
-    tags = pd.read_csv(tags_file)
+    # read in yaml file
+    with open(tags_file, "r") as f:
+        tags = pd.json_normalize(safe_load(f))
     tags_by_type = tags.groupby(by="type")
     tex_output = ""
     for tag_type in tag_types:
@@ -137,17 +141,20 @@ def fill_template(template: Path, data: dict, output: Path):
     output.write_text(temp.render(data))
 
 
-def csv_to_tex(section, data_dir, tex_dir):
-    csv_file = data_dir / f"{section}-Tabelle 1.csv"
+def yaml_to_tex(section: str, data_dir: Path, tex_dir: Path) -> Path:
+    yaml_file = data_dir / f"{section}.yaml"
     tex_file = tex_dir / f"{section}.tex"
-    data = pd.read_csv(csv_file)
+    with open(yaml_file, "r") as f:
+        data = pd.json_normalize(safe_load(f))
+    data = data.where(pd.notnull(data), None)
     data[["start", "end"]] = data[["start", "end"]].apply(pd.to_datetime)
+    data = data.applymap(lambda x: clean_string(x) if isinstance(x, str) else x)
     data = data.sort_values("start", ascending=False)
     latex_command = LATEX_COMMANDS[section]
     data["tex_code"] = data.apply(row_to_tex_code,
                                   axis=1,
                                   latex_command=latex_command)
-    tex_output = "\n".join(data.tex_code.values)
+    tex_output = "\n\\medskip".join(data.tex_code.values)
     with open(tex_file, "w") as text_file:
         text_file.write(tex_output)
     print(f"wrote {tex_file}")
@@ -172,83 +179,101 @@ def make_cvtag(row):
 
 def make_cvproject(row):
     title = f"\\cvproject{{ {row.title} }}{{| {row.subtitle} }}"
-    if pd.isnull(row.end):
-        when = f"{{ {row.start:%m/%Y} -- now  }}"
+    when = format_time_period(row.start, row.end)
+    if row.urls:
+        # pandas saves a dict in a list with one element
+        links = linkdict_to_texcode(row.urls[0])
+        links = "{{" + links + "}}"
     else:
-        when = f"{{ {row.start:%m/%Y} -- {row.end:%m/%Y}  }}"
-    row = row.replace(pd.NA, None)
-    link_columns = [col for col in row.index if col.startswith("url")]
-    links = "{{"
-    for col in link_columns:
-        url = row[col]
-        url = shorten_url(url)
-        icon_name = get_icon_for_link(url)
-        links += f"\\printinfo{{ \\{icon_name} }}{{{url}}}[{url}]"
-    links += "}}"
-    check_for_duplicate_icons(links)
-    description_cols = [col for col in row.index if col.startswith("descr")]
-    what = "\\begin{itemize}"
-    for col in description_cols:
-        description_content = row[col]
-        if description_content:
-            what += "\n"
-            what += f"\\item {row[col]}"
-    what += "\n"
-    what += "\\end{itemize}"
-    tag_columns = [col for col in row.index if col.startswith("tag")]
-    tags = "\\quad"
-    for col in tag_columns:
-        tag_content = row[col]
-        if tag_content:
-            tags += f"\\cvtag{{ {tag_content} }}"
-    tags += "\\newline"
+        links = "{}"
     cv_project = title + when + links
-    cv_project += "\n"
-    cv_project += what
-    cv_project += "\n"
-    cv_project += tags
-    # cv_project = put_in_pagebreakfree_section(cv_project)
+    what = list_to_tex_list(row.description)
+    tags = taglist_to_texcode(row.tag)
+    tags += "\\newline"
+    punchline = clean_string(row.punchline, mandatory_suffix="!")
+    punchline = enclose_in_tex_environment(punchline, "quote")
+    cv_project = "\n".join([cv_project, punchline, what, tags])
     return cv_project
 
 
 def make_cvevent(row):
     title = f"\\cvevent{{ {row.title} }}{{| {row.employee} }}"
-    if pd.isnull(row.end):
-        when = f"{{ {row.start:%m/%Y} -- now  }}"
-    else:
-        when = f"{{ {row.start:%m/%Y} -- {row.end:%m/%Y}  }}"
-    row = row.replace(pd.NA, None)
-    where = f"{{ {row.location} }}" if row.location else "{}"
-    industry = f"{{ {row.industry} }}" if row.industry else "{}"
-    description_cols = [col for col in row.index if col.startswith("descr")]
-    what = "\\begin{itemize}"
-    for col in description_cols:
-        description_content = row[col]
-        if description_content:
-            what += "\n"
-            what += f"\\item {row[col]}"
-    what += "\n"
-    what += "\\end{itemize}"
-    tag_columns = [col for col in row.index if col.startswith("tag")]
-    tags = "\\quad"
-    for col in tag_columns:
-        tag_content = row[col]
-        if tag_content:
-            tags += f"\\cvtag{{ {tag_content} }}"
-    tags += "\\newline"
+    when = format_time_period(row.start, row.end)
+    location = getattr(row, "location", "")
+    where = f"{{ {location} }}" if location else "{}"
+    industry = getattr(row, "industry", "")
+    industry = f"{{ {industry} }}" if industry else "{}"
     cv_event = title + when + where + industry
-    cv_event += "\n"
-    cv_event += what
-    cv_event += "\n"
-    cv_event += tags
-    # cv_event = put_in_pagebreakfree_section(cv_event)
+    what = list_to_tex_list(row.description)
+    tags = taglist_to_texcode(row.tag)
+    tags += "\\newline"
+    if hasattr(row, "punchline"):
+        punchline = clean_string(row.punchline, mandatory_suffix="!")
+        punchline = enclose_in_tex_environment(punchline, "quote")
+        cv_event = "\n".join([cv_event, punchline, what, tags])
+    else:
+        cv_event = "\n".join([cv_event, what, tags])
     return cv_event
+
+
+def clean_string(string: str, mandatory_suffix=None) -> str:
+    string = string.strip()
+    string = sub(r"\s+", " ", string)
+    if mandatory_suffix:
+        if not string.endswith(mandatory_suffix):
+            string += mandatory_suffix
+    return string
+
+
+def format_time_period(start, end):
+    if pd.isnull(end):
+        return f"{{ {start:%m/%Y} -- now }}"
+    else:
+        return f"{{ {start:%m/%Y} -- {end:%m/%Y} }}"
+
+
+def list_to_tex_list(data: list) -> str:
+    if not data:
+        return ""
+    tex_list = "\\begin{itemize}"
+    for item in data:
+        item = clean_string(item, mandatory_suffix=".")
+        tex_list += f"\n\\item {item}"
+    tex_list += "\n\\end{itemize}"
+    return tex_list
+
+
+def taglist_to_texcode(data: list) -> str:
+    if not data:
+        return ""
+    tex_list = ""
+    for item in data:
+        tex_list += f"\\cvtag{{ {item} }}"
+    return tex_list
+
+
+def linkdict_to_texcode(data: dict) -> str:
+    if not data:
+        return ""
+    tex_list = ""
+    for link_type, url in data.items():
+        url = shorten_url(url)
+        icon_name = get_icon_for_link(link_type)
+        tex_list += f"\\printinfo{{ \\{icon_name} }}{{{url}}}[{url}]"
+    return tex_list
 
 
 def put_in_pagebreakfree_section(tex_code):
     output = '\\begin{breakfreeunit}\n'
     output += tex_code
     output += '\n\\end{breakfreeunit}'
+    return output
+
+
+def enclose_in_tex_environment(tex_code, environment):
+    output = f'\\begin{{{environment}}}\n'
+    output += tex_code
+    output += f'\n\\end{{{environment}}}'
     return output
 
 
